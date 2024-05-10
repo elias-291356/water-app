34287b162c99261ca1d059ad9676f35646ae0c4b
elias-291356 <ikucher.002@gmail.com>
2024-05-10 18:59:48 +0400

    created AuthForm component

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div>
+      <h1>SignupPage</h1>
+      <AuthForm />
+    </div>
+  );
 };
 
 export default SignupPage;
