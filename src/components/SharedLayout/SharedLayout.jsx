38efb5d4ba15dc83a28d6106e202b121107b91b5
elias-291356 <ikucher.002@gmail.com>
2024-05-10 17:54:23 +0400

    Prepared the page to work

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import WelcomePage from "../../pages/WelcomePage/WelcomePage";
 import Header from "../Header/Header";
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
+      <WelcomePage />
     </>
   );
 };
---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WaterСonsumptionTracker from "../../components/WaterСonsumptionTracker/WaterСonsumptionTracker";
+import WhyDrinkWater from "../../components/WhyDrinkWater/WhyDrinkWater";
+const WelcomePage = () => {
+  return (
+    <>
+      <h1>WelcomePage</h1>
+      <WaterСonsumptionTracker />
+      <WhyDrinkWater />
+    </>
+  );
+};
+
+export default WelcomePage;
