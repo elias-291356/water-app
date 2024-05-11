bb761d2e17ebcf376cf28e7c4f4e6241cd25de21
elias-291356 <ikucher.002@gmail.com>
2024-05-11 23:40:11 +0400

    Preparation to background

--- src/pages/WelcomePage/WelcomePageStyled.js
+++ src/pages/WelcomePage/WelcomePageStyled.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 
-export const WelcomePageMainWrapper = styled.main`
+export const StyledWelcomePageMainWrapper = styled.main`
+  /* background-image: url(../src/images/bcg-mob.jpg),
+    url(../src/images/Vector-1.png), url(../src/images/Vector-2.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover; */
+
+  @media screen and (min-width: 768px) {
+  }
   @media screen and (min-width: 1280px) {
     justify-content: center;
     padding-top: 80px;
@@ -8,3 +16,11 @@ export const WelcomePageMainWrapper = styled.main`
     column-gap: 80px;
   }
 `;
+// export const StyledBacgroundWrap = styled.div`
+//   background-image: url(../src/images/bcg-mob.jpg),
+//     url(../src/images/Vector-1.png), url(../src/images/Vector-2.png);
+//   background-repeat: no-repeat;
+//   background-size: cover;
+//   background-position: center bottom, /* Первое изображение */ center bottom,
+//     /* Второе изображение */ center bottom; /* Третье изображение */
+// `;
