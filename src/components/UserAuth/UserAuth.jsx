86d0cd0b362690bb26ac817fde835c95111d7be0
elias-291356 <ikucher.002@gmail.com>
2024-05-19 17:00:20 +0400

    logout btn

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -6,24 +6,48 @@ import {
 } from "./UserAuthStyled";
 import sprite from "../../images/sprite.svg";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsLogin } from "../../redux/selectors";
+import { logoutThunk } from "../../redux/thunk";
 
 const UserAuth = () => {
   const navigate = useNavigate();
+  const isLogin = useSelector(selectIsLogin);
+  const dispatch = useDispatch();
 
   const onClickToSigninPage = () => {
     navigate("/signin");
   };
+
+  const onHandleLogOut = () => {
+    dispatch(logoutThunk());
+  };
   return (
     <>
-      <StyledHeaderUserItem>
-        <StyledHeaderUserItemLink onClick={onClickToSigninPage}>
-          Sign in
-        </StyledHeaderUserItemLink>
+      {isLogin ? (
+        <StyledHeaderUserItem>
+          <StyledHeaderUserItemLink onClick={onClickToSigninPage}>
+            Sign in
+          </StyledHeaderUserItemLink>
 
-        <StyledHeaderUser>
-          <use href={`${sprite}#icon-user`}></use>
-        </StyledHeaderUser>
-      </StyledHeaderUserItem>
+          <StyledHeaderUser>
+            <use href={`${sprite}#icon-user`}></use>
+          </StyledHeaderUser>
+          <button onClick={onHandleLogOut} type="button">
+            log out
+          </button>
+        </StyledHeaderUserItem>
+      ) : (
+        <StyledHeaderUserItem>
+          <StyledHeaderUserItemLink onClick={onClickToSigninPage}>
+            Sign in
+          </StyledHeaderUserItemLink>
+
+          <StyledHeaderUser>
+            <use href={`${sprite}#icon-user`}></use>
+          </StyledHeaderUser>
+        </StyledHeaderUserItem>
+      )}
     </>
   );
 };
