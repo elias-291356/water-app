94b869f0d7aeb7218404763dd634e96c6afe5f11
elias-291356 <ikucher.002@gmail.com>
2024-05-26 13:02:11 +0400

    addaptive login page

--- src/pages/SigninPage/SigninPageStyled.js
+++ src/pages/SigninPage/SigninPageStyled.js
@@ -16,6 +16,9 @@ export const StyledInputInput = styled.input`
   @media screen and (min-width: 768px) {
     width: 336px;
   }
+  @media screen and (min-width: 1280px) {
+    width: 384px;
+  }
 `;
 export const StyledALoginGoogle = styled.a`
   font-weight: 700;
@@ -27,3 +30,68 @@ export const StyledALoginGoogle = styled.a`
   color: tomato;
 `;
 
+export const StyledBannerSvgLogin = styled.svg`
+  width: 280px;
+  height: 210px;
+  z-index: -1;
+  @media screen and (min-width: 768px) {
+    position: absolute;
+    right: -220px;
+    top: 0px;
+    width: 800px;
+    height: 560px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    top: -121px;
+    width: 872px;
+    height: 661px;
+  }
+`;
+export const StyledFormWrapperLogin = styled.div`
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1280px) {
+    display: flex;
+    flex-direction: column;
+    width: 384px;
+  }
+`;
+
+export const StyledBannerSvgWrapper = styled.div`
+  width: 280px;
+  height: 210px;
+  @media screen and (min-width: 768px) {
+    position: relative;
+    width: 680px;
+    height: 550px;
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
+// export const StyledFormWrapperSignIn = styled.div`
+//   @media screen and (min-width: 768px) {
+//     position: relative;
+//     /* width: 680px;
+//     height: 550px; */
+//   }
+
+//   @media screen and (min-width: 1280px) {
+//   }
+// `;
+
+export const StyledWrapperSignIn = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 30px;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+  }
+
+  @media screen and (min-width: 1280px) {
+    flex-direction: row-reverse;
+    justify-content: space-between;
+  }
+`;
