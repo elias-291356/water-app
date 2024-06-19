2b370f15993e5a7c91ba4ad03d8dc31714ef6854
elias-291356 <ikucher.002@gmail.com>
2024-06-19 17:02:05 +0400

    base name

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -4,17 +4,47 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import {
   StyledDateCalendar,
   StyledPickersCalendarHeader,
+  // StyledButtonBase,
+  // StyledPickersDay,
 } from "./CalendarStyled";
+// import ButtonBase from "@mui/material/ButtonBase/ButtonBase";
 
+import styled, { ThemeProvider } from "styled-components";
+import { unstable_styleFunctionSx } from "@mui/system";
+import { createTheme } from "@mui/material/styles";
+const theme = createTheme();
+
+// const Div = styled("div")(unstable_styleFunctionSx);
 const Calendar = () => {
   return (
     <>
+      {/* <ThemeProvider theme={theme}> */}
+      {/* <Div sx={{ m: 1, p: 1, border: 1 }}>Custom component with the sx prop</Div> */}
+      {/* </ThemeProvider> */}
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <StyledDateCalendar
           components={{
             CalendarHeader: StyledPickersCalendarHeader,
+            // ButtonBase: StyledButtonBase,
+            // PickersDay: StyledPickersDay,
+            // Day: (props) => (
+            //   <ThemeProvider theme={theme}>
+            //     <ButtonBase
+            //       {...props}
+            //       sx={{ width: 30, height: 30, border: 1 }}
+            //     />
+            //   </ThemeProvider>
+            // ),
           }}
         />
+        {/* <ButtonBase
+          sx={{
+            width: 30,
+            height: 30,
+            border: 2,
+            backgroundColor: " #353585",
+          }}
+        /> */}
       </LocalizationProvider>
     </>
   );
--- src/components/Calendar/CalendarStyled.js
+++ src/components/Calendar/CalendarStyled.js
@@ -7,17 +7,60 @@ import { dayPickerClasses } from "@mui/x-date-pickers/DateCalendar/dayCalendarCl
 import { PickersCalendarHeader } from "@mui/x-date-pickers/PickersCalendarHeader";
 import { pickersCalendarHeaderClasses } from "@mui/x-date-pickers/PickersCalendarHeader/pickersCalendarHeaderClasses";
 
+// import ButtonBase from "@mui/material/ButtonBase/ButtonBase";
+// import buttonBaseClasses from "@mui/material/ButtonBase/buttonBaseClasses";
+
+// import { PickersDay } from "@mui/x-date-pickers/DatePicker/components/PickersDay/PickersDay";
+// import { pickersDayClasses } from "@mui/x-date-pickers/DatePicker/components/PickersDay/pickersDayClasses";
+import { PickersDay } from "@mui/x-date-pickers/PickersDay/PickersDay";
+import { pickersDayClasses } from "@mui/x-date-pickers/PickersDay/pickersDayClasses";
+
 export const StyledDateCalendar = styled(DateCalendar)`
   &.${dateCalendarClasses.root} {
     width: 280px;
+    height: 280px;
     background-color: var(--secondary-light-blue);
   }
   .${dayPickerClasses.header} {
     display: none;
   }
+
+  & .MuiButtonBase-root.MuiPickersDay-root.Mui-selected:hover {
+    background-color: var(--secondary-orange);
+  }
+  /* & .MuiButtonBase-root.MuiPickersDay-root:not(.Mui-selected) {
+    border: 1px solid red;
+  } */
 `;
 export const StyledPickersCalendarHeader = styled(PickersCalendarHeader)`
   &.${pickersCalendarHeaderClasses.root} {
     display: none;
   }
 `;
+// export const StyledButtonBase = styled(ButtonBase)`
+//   .${buttonBaseClasses.root} {
+//     width: 50px;
+//     height: 50px;
+//     background-color: red;
+//     border: 2px solid green;
+//     border-radius: 20%;
+//   }
+// `;
+// export const StyledPickersDay = styled(PickersDay)`
+//   .${pickersDayClasses.dayWithMargin} {
+//     width: 50px;
+//     height: 50px;
+//     background-color: red;
+//     border: 2px solid green;
+//     border-radius: 50px;
+//     margin-top: 20px;
+//   }
+
+//   .${pickersDayClasses.day} {
+//     width: 50px;
+//     height: 50px;
+//     background-color: red;
+//     border: 2px solid green;
+//     border-radius: 50px;
+//   }
+// `;
